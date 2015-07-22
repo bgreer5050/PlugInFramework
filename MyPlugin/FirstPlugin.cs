2c001f543d694b203448730cc197676ebc46f203
bgreer5050 <bgreer5050@gmail.com>
2015-07-21 21:25:39 -0500

    InitialCommit

---
+++ MyPlugin/FirstPlugin.cs
@@ -0,0 +1,24 @@
+﻿using PluginContracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyPlugin
+{
+    public class FirstPlugin :IPlugin
+    {
+        public string Name
+        {
+            get {
+                return "From Bills Plugin";
+            }
+        }
+
+        public void Do()
+        {
+            Console.WriteLine(this.Name);
+        }
+    }
+}
---
+++ Plugin.Console/Program.cs
@@ -0,0 +1,70 @@
+﻿using PluginContracts;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plugin.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LoadPlugins();
+            System.Console.ReadLine();
+        }
+
+        private static void LoadPlugins()
+        {
+            if (!Directory.Exists("PluginDirectory"))
+            {
+                Directory.CreateDirectory("PluginDirectory");
+                System.Console.WriteLine("Plugin Directory Created");
+            }
+            else
+            {
+                System.Console.WriteLine("Plugin Directory Exists");
+
+            }
+
+            //Get dlls from plugin directory
+            string[] dllFileNames = null;
+            dllFileNames = Directory.GetFiles("PluginDirectory");
+
+            //load the assemblies
+            ICollection<Assembly> assemblies = new List<Assembly>(dllFileNames.Length);
+
+            System.Console.WriteLine("Loading {0} Assemblies.", dllFileNames.Length.ToString());
+
+            foreach(string dllFile in dllFileNames)
+            {
+                AssemblyName an = System.Reflection.AssemblyName.GetAssemblyName(dllFile);
+                Assembly assembly = Assembly.Load(an);
+                assemblies.Add(assembly);
+            }
+
+            System.Console.WriteLine("Search For Assemblies That Match Our Interface");
+
+            Type pluginType = typeof(IPlugin);
+            ICollection<Type> pluginTypes = new List<Type>();
+            foreach(Assembly assembly in assemblies)
+            {
+                if(assembly != null)
+                {
+                    Type[] types = assembly.GetTypes();
+                    throw new NotImplementedException();
+                }
+            }
+
+
+
+
+            
+
+
+        }
+    }
+}
